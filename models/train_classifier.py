e6b3f952e84fedc4f61eca0218d2982477ce5346
Tarciso Braz <tarcisocomp>
2020-06-16 17:41:19 -0300

    refactor: Adapts script to use Local W2V estimator.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -20,20 +20,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 import joblib
 
+#Custom Transformers and Estimators
+from nlp_estimators import *
+
 def load_data(database_filepath):
     engine = create_engine('sqlite:///' + database_filepath)
-    df = pd.read_sql_table(con=engine, table_name='Message')
-    X = df.message.values
-    Y_df = df.drop(['id','message','original','genre'], axis=1)
-    Y = Y_df.values
-    category_names = Y_df.columns
+    messages_df = pd.read_sql_table(con=engine, table_name='Message')
+    categories_df = pd.read_sql_table(con=engine, table_name='CorpusWide')
+    messages_tokens = pd.read_sql_table(con=engine, table_name='MessageTokens')
     
-    return X, Y, category_names
+    X = messages_df.message.values
+    X_tokenized = messages_tokens.tokens_str.values
+    Y_df = categories_df.drop(['message_id', 'message', 'original', 'genre'], axis=1)
+    Y = Y_df.values
+    category_columns = Y_df.columns
+    
+    return X, X_tokenized, Y, category_columns
 
 
 def tokenize(text):
@@ -44,33 +52,63 @@ def tokenize(text):
     return clean_tokens
 
 
-def build_model():
-    pipeline = Pipeline([
-        ('vect',CountVectorizer(tokenizer=tokenize, ngram_range=(1,2))),
-        ('tfidf', TfidfTransformer()),
-        ('multi_clf', MultiOutputClassifier(RandomForestClassifier(random_state=199)))
-    ])
+def build_model(model_config):
+    feature_set = model_config['feature_set']
+    pipeline = grid_search_params = grid_search_cv = None
+    jobs = -1
+    score = 'f1_micro'
+    def_cv = 3
+    verbosity_level=10
     
-    parameters = {
-        #'multi_clf__estimator__n_estimators': [20,50],
-        #'multi_clf__estimator__max_depth': [50, 100]
-    }
-
-    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=3)
+    if feature_set == 'local_w2v':
+        pipeline = Pipeline([
+                            ('local_w2v', TfidfEmbeddingTrainVectorizer()),
+                            ('clf', MultiOutputClassifier(GaussianNB()))
+                        ])
+        
+        grid_search_params = {'local_w2v__num_dims' : [50]}
+        
+#         params_options_models_simple = [MultiOutputClassifier(RandomForestClassifier(random_state=199,
+#                                                         n_estimators=50,
+#                                                         max_depth=100,
+#                                                         min_samples_split=5)),
+#                                             MultiOutputClassifier(LogisticRegression(random_state=199,
+#                                                                                     solver='liblinear',
+#                                                                                     C=1,
+#                                                                                     penalty='l2'))]
+        
+#         grid_search_params = {'local_w2v__num_dims' : [50,100,300],
+#                               'clf' : params_options_models_simple}
+        
+    else:
+        print("Error: Invalid Feature Set: " + feature_set)
+        sys.exit(1)
+        
+    grid_search_cv = GridSearchCV(estimator=pipeline,
+            param_grid=grid_search_params,
+            scoring=score,
+            cv=def_cv,
+            n_jobs=jobs,
+            verbose=verbosity_level)
     
-    return cv
+    return grid_search_cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    # predict on test data
-    Y_pred = model.predict(X_test)
+    print('Best params: %s' % model.best_params_)
+    # Best training data accuracy
+    print('Best training score: %.3f' % model.best_score_)
+    # Predict on test data with best params
+    test_score = model.score(X_test, Y_test)
+    # Test data accuracy of model with best params
+    print('Test set score for best params: %.3f ' % test_score)    
     
-    for category_idx in range(len(category_names)):
-        print(classification_report(y_pred=Y_pred[:,category_idx],
-                                    y_true=Y_test[:,category_idx], 
-                                    labels=[0,1], 
-                                    target_names=[category_names[category_idx] + '-0',
-                                                  category_names[category_idx] + '-1']))
+#     for category_idx in range(len(category_names)):
+#         print(classification_report(y_pred=Y_pred[:,category_idx],
+#                                     y_true=Y_test[:,category_idx], 
+#                                     labels=[0,1], 
+#                                     target_names=[category_names[category_idx] + '-0',
+#                                                   category_names[category_idx] + '-1']))
 
 
 def save_model(model, model_filepath):
@@ -82,11 +120,11 @@ def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, X_tokenized, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X_tokenized, Y, test_size=0.25)
         
         print('Building model...')
-        model = build_model()
+        model = build_model({'feature_set':'local_w2v'})
         
         print('Training model...')
         model.fit(X_train, Y_train)
@@ -95,7 +133,7 @@ def main():
         evaluate_model(model, X_test, Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        save_model(model.best_estimator_, model_filepath)
 
         print('Trained model saved!')
 
